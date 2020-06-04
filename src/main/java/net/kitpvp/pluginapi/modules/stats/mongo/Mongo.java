13f765ef86ca30600392d1f922836f06dd42b6fe
f <moritz.hennen@gmail.com>
2020-06-04 22:08:43 +0200

    Add more Mongo#find methods

--- src/main/java/net/kitpvp/pluginapi/modules/stats/mongo/Mongo.java
+++ src/main/java/net/kitpvp/pluginapi/modules/stats/mongo/Mongo.java
@@ -3,10 +3,23 @@ package net.kitpvp.pluginapi.modules.stats.mongo;
 import com.mongodb.client.MongoIterable;
 import net.kitpvp.mongodbapi.MongoCollection;
 import net.kitpvp.pluginapi.modules.stats.mongo.find.Comparison;
+import net.kitpvp.pluginapi.modules.stats.mongo.statskeys.SStatsKey;
 import net.kitpvp.pluginapi.modules.stats.mongo.statskeys.StatsKey;
 import org.bson.Document;
 
 public class Mongo {
+
+    public static <V> MongoStatsReader findOne(MongoCollection collection, SStatsKey<V> statsKey, V v) {
+        return findOne(collection, statsKey, Comparison.EQUALS, v);
+    }
+
+    public static <V> MongoStatsReader findOne(MongoCollection collection, SStatsKey<V> statsKey, Comparison comparison, V v) {
+        return findOne(collection, statsKey, comparison, null, v);
+    }
+
+    public static <K, V> MongoStatsReader findOne(MongoCollection collection, StatsKey<K, V> statsKey, K k, V v) {
+        return findOne(collection, statsKey, Comparison.EQUALS, k, v);
+    }
 
     public static <K, V> MongoStatsReader findOne(MongoCollection collection, StatsKey<K, V> statsKey, Comparison comparison, K k, V v) {
         return find(collection, statsKey, comparison, k, v).first();
